cef8551540644d3a31c70f02d79c3b561a99cead
Max W <87267714+FuriousF0xGG@users.noreply.github.com>
2022-04-22 21:30:58 +0100

    updated fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 author 'Max W.#0001, thanks to tazio and badger as I did take some code from there staff duty script and PoliceEMSActivity script'
 description 'A script to go on duty'
 version '0.1'
-url 'No_link'
+url 'https://github.com/FuriousF0xGG/M-Duty'
 
 client_scripts {
 	'client.lua',
