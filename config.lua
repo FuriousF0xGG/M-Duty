9bad7b39e283da1b234dbc7802633bca7aa824a1
Max W <87267714+FuriousF0xGG@users.noreply.github.com>
2022-04-26 20:56:09 +0100

    updated config.lua so some things are fancy

--- config.lua
+++ config.lua
@@ -1,2 +1,15 @@
 --[[ EVERYTHING IN HERE WILL BE UPCOMING, DO NOT CONFIGURE ANY OF THIS AS IT WILL NOT DO ANYTHING
 hudcoordinates = {x= 2, y = 64, z = 257} --upcoming hud 👀]]
+
+Config = {}
+
+Config = {
+    Duty_Command = "duty", --Set the command you want to go onduty as an LEO here
+    Civ_Duty_Command = "civ", --Set the command you want to go onduty as a Civillian
+    EMS_Duty_Command = "emsduty", --Set the command you want to go onduty as a Civillian Here
+
+--Hud Coords (Coming soon)
+--[[
+X_Coordinates = COOMING_SOON,
+Y_Coordinates = COMMING_SOON,]]
+}
